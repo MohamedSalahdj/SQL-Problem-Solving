0ef0291462cdc76d9232955097eb3ad7694fd10e
MohamedSalahdj <mohamedsamydj@gmail.com>
2024-06-09 16:08:04 +0300

    added african-cities , japan-population problems

---
+++ HackerRANK/african-cities.sql
@@ -0,0 +1,14 @@
+/*
+    Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'
+    Note: CITY.CountryCode and COUNTRY.Code  are matching key columns.
+
+*/
+
+
+SELECT 
+    CITY.NAME
+FROM 
+    CITY 
+INNER JOIN COUNTRY 
+ON CITY.COUNTRYCODE = COUNTRY.CODE 
+WHERE COUNTRY.CONTINENT = 'Africa' ;
