195576ac9993a569805f1915d1dfa3eab4e55485
MohamedSalahdj <mohamedsamydj@gmail.com>
2024-06-10 12:39:45 +0300

    added weather-observation-station-8, weather-observation-station-11 problems

--- HackerRANK/weather-observation-station-8.sql
+++ HackerRANK/weather-observation-station-8.sql
@@ -1,8 +1,13 @@
 /*
     Query the list of CITY  names from STATION which have vowels (i.e.. a, e, i, o or u) as both their first last characters.
     Your result cannot contain duplicates.
-
 */
 
-
-
+SELECT
+    DISTINCT CITY
+FROM
+    STATION
+WHERE 
+    (CITY LIKE 'A%' OR CITY LIKE 'E%' OR CITY LIKE 'I%' OR CITY LIKE 'O%' OR CITY LIKE 'U%' )
+    AND 
+    (CITY LIKE '%a' OR CITY LIKE '%e' OR CITY LIKE '%i' OR CITY LIKE '%o' OR CITY LIKE '%u' );
