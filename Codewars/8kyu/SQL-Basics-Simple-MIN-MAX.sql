b24addd28caa43db615e99466afea7fc473a0140
MohamedSalahdj <mohamedsamydj@gmail.com>
2024-06-11 09:35:32 +0300

    added SQL Basics: Simple MIN / MAX, SQL Basics: Simple DISTINCT problems

---
+++ Codewars/8kyu/SQL-Basics-Simple-MIN-MAX.sql
@@ -0,0 +1,18 @@
+/*
+    For this challenge you need to create a simple MIN / MAX statement that will return the Minimum and Maximum ages out of all the people.
+
+    people table schema
+        id
+        name
+        age
+    select table schema
+        age_min (minimum of ages)
+        age_max (maximum of ages)
+    NOTE: Your solution should use pure SQL. Ruby is used within the test cases to do the actual testing.
+*/
+
+SELECT
+  MIN(age) as age_min ,
+  MAX(age) as age_max 
+FROM
+  people
