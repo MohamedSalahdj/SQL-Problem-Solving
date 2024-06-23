e8e7fc0faa51570bcc2b24dac8c10fd3e3c42edc
MohamedSalahdj <mohamedsamydj@gmail.com>
2024-06-23 12:18:42 +0300

    added Easy SQL: Counting and Grouping problem

---
+++ Codewars/7kyu/Easy-SQL-Counting-and-Grouping.sql
@@ -0,0 +1,27 @@
+/*
+Given a demographics table in the following format:
+
+** demographics table schema **
+
+id
+name
+birthday
+race
+you need to return a table that shows a count of each race represented, ordered by the count in descending order as:
+
+** output table schema **
+
+race
+count
+
+*/
+
+SELECT
+  race,
+  count(race) as count
+FROM
+  demographics 
+GROUP BY
+  race
+ORDER BY 
+  count(race) DESC;
